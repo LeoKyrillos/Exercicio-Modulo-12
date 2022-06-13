32c597031124ad1e3241a718bbc27b093f567b98
Léo Kyrillos <leokyrillos@gmail.com>
2022-06-13 15:21:48 -0400

    Alteração após correção- Inclusão de comandos customizados

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -10,72 +10,51 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
+        cy.visit('login')
         cy.visit('produtos/')
+        
     });
 
     afterEach(() => {
-        cy.screenshot()
+        cy.screenshot()        
     });
 
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+    it('Deve adicionar o produto 1', () => {
+
+        cy.addProduto('Aether Gym Pant', '33', 'Brown', 2)
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
+        cy.get('.woocommerce-message').should('contain',  '2 × “Aether Gym Pant” foram adicionados no seu carrinho.')
         
-        var quantidade = 2
-        var quantidade1 = 1
-        var somaquant = quantidade + quantidade1
-        var quantidade2 = 1
-        var somaquant1 = quantidade + quantidade1 + quantidade2
-        var quantidade3 = 4
-        var somaquant2 = quantidade + quantidade1 + quantidade2 + quantidade3
+    });
 
+    it('Deve adicionar o produto 2', () => {
+
+        cy.addProduto('Abominable Hoodie', 'L', 'Red', 1)
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
+        cy.get('.woocommerce-message').should('contain',  '“Abominable Hoodie” foi adicionado no seu carrinho.')
         
-        cy.get('[class="product-block grid"]')
-            .contains('Aether Gym Pant').click()
-        cy.get('.button-variable-item-33').click()
-        cy.get('.button-variable-item-Brown').click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Aether Gym Pant” foram adicionados no seu carrinho.')
+    });
 
+    it('Deve adicionar o produto 3', () => {
+
+        cy.addProduto('Aero Daily Fitness Tee', 'S', 'Yellow', 1)
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
+        cy.get('.woocommerce-message').should('contain',  '“Aero Daily Fitness Tee” foi adicionado no seu carrinho.')
         
-    
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get('[class="product-block grid"]')
-            .contains('Abominable Hoodie').click()
-        cy.get('.button-variable-item-L').click()
-        cy.get('.button-variable-item-Red').click()
-        cy.get('.input-text').clear().type(quantidade1)
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', somaquant)
-        cy.get('.woocommerce-message').should('contain', ' “Abominable Hoodie” foi adicionado no seu carrinho.')
+    });
 
-   
+    it('Deve adicionar o produto 4', () => {
 
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get('[class="product-block grid"]')
-            .contains('Aero Daily Fitness Tee').click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Yellow').click()
-        cy.get('.input-text').clear().type(quantidade2)
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', somaquant1)
-        cy.get('.woocommerce-message').should('contain', ' “Aero Daily Fitness Tee” foi adicionado no seu carrinho.')
-        
-   
-
-        cy.get('#primary-menu > .menu-item-629 > a').click()
         cy.get(':nth-child(8) > .page-numbers').click()
-        cy.get('[class="product-block grid"]')
-            .contains('Zoltan Gym Tee').click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Blue').click()
-        cy.get('.input-text').clear().type(quantidade3)
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', somaquant2)
-        cy.get('.woocommerce-message').should('contain', quantidade3 + ' × “Zoltan Gym Tee” foram adicionados no seu carrinho.')
-        
-    
+        cy.addProduto('Zoltan Gym Tee', 'S', 'Blue', 4)
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
+        cy.get('.woocommerce-message').should('contain',  '4 × “Zoltan Gym Tee” foram adicionados no seu carrinho.')
+
         cy.get('.top-cart-wishlist').click()
         cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
         cy.get('[type="checkbox"]').check({ force: true })
@@ -88,10 +67,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('#terms').click()
         cy.get('#place_order').click({ force: true })
 
-        cy.get('.woocommerce-notice').contains('Obrigado. Seu pedido foi recebido.')
-
-        
-
+        cy.get('.woocommerce-notice').contains('Obrigado. Seu pedido foi recebido.')        
     });
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,7 +27,18 @@
 
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
 });
+
+Cypress.Commands.add('addProduto', (produto, tamanho, cor, quantidade,) => {
+    cy.get('[class="product-block grid"]')
+        .contains(produto).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-'  + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+
+
+})
 
