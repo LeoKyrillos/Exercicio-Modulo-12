bae7aec606d83e7a2a1f915af1a4bd68751cf83e
Léo Kyrillos <leokyrillos@gmail.com>
2022-06-14 12:37:27 -0400

    Alterações após orientação

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -10,8 +11,8 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('login')
         cy.visit('produtos/')
+             
         
     });
 
@@ -20,40 +21,16 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
 
-    it('Deve adicionar o produto 1', () => {
+    it('Deve adicionar produtos no carrinh de compra e finalizar a compra', () => {
 
         cy.addProduto('Aether Gym Pant', '33', 'Brown', 2)
 
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
-        cy.get('.woocommerce-message').should('contain',  '2 × “Aether Gym Pant” foram adicionados no seu carrinho.')
-        
-    });
-
-    it('Deve adicionar o produto 2', () => {
-
         cy.addProduto('Abominable Hoodie', 'L', 'Red', 1)
-
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
-        cy.get('.woocommerce-message').should('contain',  '“Abominable Hoodie” foi adicionado no seu carrinho.')
-        
-    });
-
-    it('Deve adicionar o produto 3', () => {
 
         cy.addProduto('Aero Daily Fitness Tee', 'S', 'Yellow', 1)
 
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
-        cy.get('.woocommerce-message').should('contain',  '“Aero Daily Fitness Tee” foi adicionado no seu carrinho.')
-        
-    });
-
-    it('Deve adicionar o produto 4', () => {
-
         cy.get(':nth-child(8) > .page-numbers').click()
         cy.addProduto('Zoltan Gym Tee', 'S', 'Blue', 4)
-
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
-        cy.get('.woocommerce-message').should('contain',  '4 × “Zoltan Gym Tee” foram adicionados no seu carrinho.')
 
         cy.get('.top-cart-wishlist').click()
         cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
